46f657a23119b74cec4fccae086c65d9fb2f8d9f
Seyed Mojtaba Esmaili <mo93esm@gmail.com>
2022-06-20 12:26:11 +0430

    menu function created

--- sniffer.py
+++ sniffer.py
@@ -1,2 +1,24 @@
 from time import sleep
 from scapy.all import *
+
+
+def menu():
+    while True:
+        print("Welcom,What do you want??\n\n\n")        
+        print("          1) Sniff everything!\n")       # 10 spaces-
+        print("          2) Sniff a protocol!\n\n\n")
+
+        inp = int(input("(0 to Exit) : "))
+        if inp == 1:
+            sniff_all()
+        elif inp == 2:
+            print("which one?\n\n\n")
+            print("    1) TCP\n")  # 4 spaces
+            print("    2) UDP\n")
+            print("    3) ICMP\n")
+            protocol = int(input(" : "))
+            sniff_protocol(protocol)
+        elif inp == 0:
+            quit()
+        else:
+            print("Ops, enter the right numbers :D")
