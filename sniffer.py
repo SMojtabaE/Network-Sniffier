6e7b416ce0a9da7b3df6920f32a8a2867c8505f1
Seyed Mojtaba Esmaili <mo93esm@gmail.com>
2022-06-20 12:27:13 +0430

    sniff_all function maded

--- sniffer.py
+++ sniffer.py
@@ -2,6 +2,28 @@ from time import sleep
 from scapy.all import *
 
 
+def sniff_all():
+    count =int(input("\n\n\nEnter number of packets you want : "))
+    pck = sniff(count=count)
+    print(pck)
+    inp = input("want the detales?(y/n)")
+    if inp == "y":
+        print(pck.show())
+        numberOFpck = int(input("which one do you want to see ? "))
+
+        if "UDP" in pck[numberOFpck]:
+            UDP(pck[numberOFpck])
+        elif "TCP" in pck[numberOFpck]:
+            TCP(pck[numberOFpck])
+        elif "ICMP" in pck[numberOFpck]:
+            ICMP(pck[numberOFpck])
+        else:
+            print("tha package is not TCP or UDP or ICMP...")
+            print("\nopening package...")
+            sleep(2)
+            print(pck[numberOFpck].show())
+            input()
+
 def menu():
     while True:
         print("Welcom,What do you want??\n\n\n")        
